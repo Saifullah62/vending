f3ab48cb59028926dbf04731472de597c9176d5a
GotThatData <bwd@ugherty.net>
2024-11-17 00:33:15 -0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js'; // Note: Add .js for ESM
 import config from '../config/config.js'; // Note: Add .js for ESM
