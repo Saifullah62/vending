9ee8ed737c2d99c6b909f34ce1e5c86799e11af1
GotThatData <bwd@ugherty.net>
2024-11-16 23:52:50 -0500

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,10 +1,30 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
+/**
+ * Transaction Schema for MongoDB using Mongoose.
+ * This schema defines the structure of a transaction document in the database.
+ */
 const TransactionSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    share_id: { type: mongoose.Schema.Types.ObjectId, ref: 'ShareInventory', required: true },
-    transaction_type: { type: String, required: true },
-    amount: { type: Number, required: true }
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    shareId: { type: mongoose.Schema.Types.ObjectId, ref: 'ShareInventory', required: true },
+    transactionType: { type: String, enum: ['buy', 'sell'], required: true }, // Specifies the type of transaction
+    amount: { type: Number, required: true, min: 0 } // Amount must be a non-negative number
 }, { timestamps: true });
 
-module.exports = mongoose.model('Transaction', TransactionSchema);
+/**
+ * Static method to find transactions based on a query.
+ * @param {Object} query - The query object to filter transactions.
+ * @returns {Promise<Array>} - A promise that resolves to an array of transactions.
+ * @throws {Error} - Throws an error if the fetch operation fails.
+ */
+TransactionSchema.statics.findTransactions = async function(query) {
+    try {
+        return await this.find(query).lean();
+    } catch (error) {
+        console.error('Error fetching transactions:', error);
+        throw new Error('Could not fetch transactions.');
+    }
+};
+
+// Export the Transaction model based on the defined schema
+export default mongoose.model('Transaction', TransactionSchema);
