f0b3fc9eca71972b3edfa53f0b2dc39b0186003a
GotThatData <bwd@ugherty.net>
2024-11-17 01:51:38 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Register from './components/Register';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>MyCardShares Vending Machine</h1>
+            <Register />
+        </div>
+    );
+}
+
+export default App;
